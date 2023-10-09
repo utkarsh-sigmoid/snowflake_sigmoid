50d34119f3ff25a8deece54fe877781e89ff7630
utkarsh-goodyear <utkarsh_yadav@goodyear.com>
2023-10-09 11:22:54 +0530

    Add table employee in sigmoid_Schema

--- migrations/V1.1.1__initial_database_object.sql
+++
@@ -1,3 +0,0 @@
-Create SCHEMA Org
-
-CREATE OR REPLACE TABLE employees ( EMPLOYEE_ID int, FIRST_NAME varchar(25), LAST_NAME varchar(25), EMAIL varchar(25), HIRE_DATE varchar(25),  SALARY number(12,2), DEPARTMENT varchar(25) )
---
+++ migrations/V1.1.4__initial_objects.sql
@@ -0,0 +1,11 @@
+Create SCHEMA SIGMOID_SCHEMA;
+
+CREATE OR REPLACE TABLE employees (
+    EMPLOYEE_ID int,
+    FIRST_NAME varchar(25),
+    LAST_NAME varchar(25),
+    EMAIL varchar(25),
+    HIRE_DATE varchar(25),
+    SALARY number(12,2),
+    DEPARTMENT varchar(25)
+);
