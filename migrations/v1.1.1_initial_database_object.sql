8605e36727062a75f053a16d772ebccdd6c7a5f0
utkarsh-sigmoid <147089232+utkarsh-sigmoid@users.noreply.github.com>
2023-10-09 10:58:51 +0530

    Update v1.1.1_initial_database_object.sql

--- migrations/v1.1.1_initial_database_object.sql
+++ migrations/v1.1.1_initial_database_object.sql
@@ -1,3 +1,3 @@
-Create SCHEMA Org;
+Create SCHEMA Org
 
-CREATE OR REPLACE TABLE employees ( EMPLOYEE_ID int, FIRST_NAME varchar(25), LAST_NAME varchar(25), EMAIL varchar(25), HIRE_DATE varchar(25),  SALARY number(12,2), DEPARTMENT varchar(25) );
+CREATE OR REPLACE TABLE employees ( EMPLOYEE_ID int, FIRST_NAME varchar(25), LAST_NAME varchar(25), EMAIL varchar(25), HIRE_DATE varchar(25),  SALARY number(12,2), DEPARTMENT varchar(25) )
