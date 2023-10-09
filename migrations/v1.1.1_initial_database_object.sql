08005ca8bf660cd6237e033d85f5e82ca960a9e7
utkarsh-goodyear <utkarsh_yadav@goodyear.com>
2023-10-09 10:52:17 +0530

    Create Table employee

---
+++ migrations/v1.1.1_initial_database_object.sql
@@ -0,0 +1,3 @@
+USE SCHEMA DEMO;
+
+CREATE OR REPLACE TABLE employees ( EMPLOYEE_ID int, FIRST_NAME varchar(25), LAST_NAME varchar(25), EMAIL varchar(25), HIRE_DATE varchar(25),  SALARY number(12,2), DEPARTMENT varchar(25) );
